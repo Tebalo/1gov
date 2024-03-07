00ac5bd6cd9fb4b38dbacee78ba494c339c7bd02
bopaki <btebalo@gmail.com>
2024-03-08 00:20:31 +0200

    fixed the error

--- app/portal/dashboard/(officer)/home-o/registration/page.tsx
+++ app/portal/dashboard/(officer)/home-o/registration/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, {useState}  from "react";
-import {motion} from 'framer-motion';
+
 interface TeacherData {
     teacher_registrations: {
       reg_number: string;
@@ -26,28 +26,48 @@ interface TeacherRegistrationProps {
     data: TeacherData;
   }
 
-const rowData: RowData[] = [
-    { id: '67bf7nfe74unf843m', title: 'Teacher registration', date: '02 Feb 2024 - 12:48', status: 'Draft', action: '' },
-];
-
-const MyApplications: React.FC<TeacherRegistrationProps> = ({data}) => {
-    return(
+const data = {
+    "teacher_registrations":{
+        "reg_number":"GH6778888",
+        "reg_status":"Pending-Screening",
+        "registration_type":"Teacher"
+    },
+    "bio_datas":{
+        "national_id":"936510813",
+        "surname":"Serala",
+        "forenames":"Oaitse",
+        "dob":"1996-02-15",
+        "pob":"Mahalapye",
+        "gender":"Male",
+        "nationality":"Motswana",
+        "postal_address":"P O Box 7886, Mahalapye",
+        "physical_address":"Block 10, Gaborone",
+        "email":"johndoe@gmail.com",
+        "mobile":"26774217788",
+        "marital_status":"Single",
+        "next_of_kin_name":"Sarah Cornor",
+        "next_of_kin_relation":"Mother",
+        "next_of_kin_contact":"26776554321",
+        "disability":"Yes",
+        "disability_description":"left eye is impaired and dont see clearly on cold conditions"
+    },
+}
+const Page: React.FC<TeacherRegistrationProps> = ({ data }) => {
+    return (
         <main className="mx-10 mt-10 space-y-3">
             <div>
-            <span>View Teacher registration</span>
-            <p>Registration Number: {data.teacher_registrations.reg_number}</p>
-            <p>Registration Status: {data.teacher_registrations.reg_status}</p>
-            <p>Registration Type: {data.teacher_registrations.registration_type}</p>
-            
-            <h2>Bio Data</h2>
-            <p>National ID: {data.bio_datas.national_id}</p>
-            <p>Surname: {data.bio_datas.surname}</p>
-      {/* to add more bio data fields here */}
+                <span>View Teacher registration</span>
+                <p>Registration Number: {data?.teacher_registrations?.reg_number}</p>
+                <p>Registration Status: {data?.teacher_registrations?.reg_status}</p>
+                <p>Registration Type: {data?.teacher_registrations?.registration_type}</p>
 
-
+                <h2>Bio Data</h2>
+                <p>National ID: {data?.bio_datas?.national_id}</p>
+                <p>Surname: {data?.bio_datas?.surname}</p>
+                {/* to add more bio data fields here */}
             </div>
+       </main>
+    );
+};
 
-        </main>
-    )
-}
-export default MyApplications;
+export default Page; 
