20d051ab47d1d2cdb1bc9808b31ff054071dddd7
misscharles <olorato@26digitalbw.com>
2024-03-07 22:01:33 +0200

    tr changes

--- app/portal/dashboard/(officer)/home-o/registration/page.tsx
+++ app/portal/dashboard/(officer)/home-o/registration/page.tsx
@@ -1,7 +1,20 @@
 "use client"
 import React, {useState}  from "react";
 import {motion} from 'framer-motion';
-
+interface TeacherData {
+    teacher_registrations: {
+      reg_number: string;
+      reg_status: string;
+      registration_type: string;
+    };
+    bio_datas: {
+      national_id: string;
+      surname: string;
+      forenames: string;
+      dob: string;
+      // to add other fields as needed
+    };
+}
 interface RowData {
     id: string;
     title: string;
@@ -9,15 +22,31 @@ interface RowData {
     status: string;
     action: string;
 }
+interface TeacherRegistrationProps {
+    data: TeacherData;
+  }
 
 const rowData: RowData[] = [
     { id: '67bf7nfe74unf843m', title: 'Teacher registration', date: '02 Feb 2024 - 12:48', status: 'Draft', action: '' },
 ];
 
-const MyApplications: React.FC = () => {
+const MyApplications: React.FC<TeacherRegistrationProps> = ({data}) => {
     return(
         <main className="mx-10 mt-10 space-y-3">
+            <div>
             <span>View Teacher registration</span>
+            <p>Registration Number: {data.teacher_registrations.reg_number}</p>
+            <p>Registration Status: {data.teacher_registrations.reg_status}</p>
+            <p>Registration Type: {data.teacher_registrations.registration_type}</p>
+            
+            <h2>Bio Data</h2>
+            <p>National ID: {data.bio_datas.national_id}</p>
+            <p>Surname: {data.bio_datas.surname}</p>
+      {/* to add more bio data fields here */}
+
+
+            </div>
+
         </main>
     )
 }
