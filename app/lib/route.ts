135dc72df1b3d1ca080038a15da3592720e57680
bopaki <btebalo@gmail.com>
2024-03-10 13:47:43 +0200

    Added exception handling

--- app/lib/route.ts
+++ app/lib/route.ts
@@ -11,59 +11,24 @@ export default async function getHistory(reg_number: string){
   }
 
 export async function getNext(){
-    const res = await fetch('http://66.179.253.57/api/getNext/')
+    const res = await fetch('http://66.179.253.57/api/getNext/', {cache: 'no-store'})
     
-    const response = {
-        "national_id": "558714915",
-        "reg_number": "$2y$10$jbzHa2pL",
-        "reg_status": "Pending-Review",
-        "registration_type": "teacher",
-        "created_at": "2024-03-07 18:46:50",
-        "updated_at": "2024-03-07 18:46:50",
-        "id": 20,
-        "level": "diploma",
-        "qualification": "refe",
-        "institution": "dffd",
-        "qualification_year": "345",
-        "minor_subjects": "english",
-        "major_subjects": "setswana",
-        "attachments": "refe-558714915-20240307.pdf",
-        "surname": "MORWE",
-        "forenames": "LEAPEETSWE LEAH",
-        "dob": "1995-02-17T00:00:00.000Z",
-        "pob": "KANYE",
-        "gender": "M",
-        "nationality": "Motswana",
-        "postal_address": "BOX  M1585 KANYE",
-        "physical_address": "SEBELE  ",
-        "email": "johndoe@gmail.com",
-        "mobile": "26774217788",
-        "marital_status": "Single",
-        "next_of_kin_name": "Sarah Cornor",
-        "next_of_kin_relation": "Mother",
-        "next_of_kin_contact": "26776554321",
-        "disability": "yes",
-        "disability_description": "visual_impairment",
-        "agreement": "1",
-        "signature": null,
-        "citizen_status": "Citizen",
-        "work_status": "educational consultant",
-        "practice_category": "primary",
-        "sub_cateogry": null,
-        "student_related_offence": "no",
-        "student_related_offence_details": null,
-        "drug_related_offence": "no",
-        "drug_related_offence_details": null,
-        "license_flag": "no",
-        "license_flag_details": null,
-        "misconduct_flag": "no",
-        "misconduct_flag_details": null,
-        "experience_years": 0,
-        "current_institution": "marua_pula",
-        "institution_type": null,
-        "region": "gaborone",
-        "district": "chobe",
-        "city_or_town": "mahalapye"
-    }    
     return res.json()
-  }
+}
+
+interface StatusProps {
+    id: string;
+    status: string;
+}
+
+export async function UpdateStatus(id: string, status: string ): Promise<StatusProps>{
+
+    const res = await fetch(`http://66.179.253.57/api/teacher_registrations/${id}?registration_status=${status}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    return res.json()
+}
--- app/portal/dashboard/(officer)/home-o/reg/page.tsx
+++ app/portal/dashboard/(officer)/home-o/reg/page.tsx
@@ -2,6 +2,7 @@ import CaseDetails from "@/app/components/case/casedetails";
 import Utilities from "@/app/components/case/utilities";
 import WorkArea from "@/app/components/case/workarea";
 import { getNext } from "@/app/lib/route";
+import Link from "next/link";
 import React, {useState}  from "react";
 
 interface Record{
@@ -10,27 +11,47 @@ interface Record{
 
 const Page: React.FC<Record> = async () => {
     const work = await getNext()
+    
     const details = {
-        'status': work.reg_status,
-        'type': work.registration_type,
+        'status': work.reg_status ?? '',
+        'type': work.registration_type ?? '',
         'id': work.national_id,
-        'createdBy': work.forenames +' '+ work.surname,
-        'createdAt': work.created_at,
-        'updatedAt': work.updated_at,
+        'createdBy': work.forenames +' '+ work.surname ?? '',
+        'createdAt': work.created_at ?? '',
+        'updatedAt': work.updated_at ?? '',
     }
     const recordDetails = {
         'preliminary': {
-            'type': work.registration_type,
+            'type': work.registration_type ?? '',
             'work_status': work.work_status,
-            'practice_category': work.practice_category,
-            'sub_cateogry': work.sub_cateogry,
+            'practice_category': work.practice_category ?? '',
+            'sub_cateogry': work.sub_cateogry ?? '',
         }, 
     }
     return (
         <main className="h-full">
             <div className="flex flex-row h-full gap-1">
-                <CaseDetails {...details}/>
-                <WorkArea {...recordDetails}/>
+                {work===null && <CaseDetails {...details}/> &&
+                <WorkArea {...recordDetails}/>}
+                {work!==null &&
+                    <div className="w-full md:h-96 items-center flex justify-center">
+                        <div>
+                            <h2 className="text-center text-black text-3xl">Work not found!</h2>
+                            <div className="flex items-center justify-center">
+                                <Link
+                                href='/portal/dashboard/home-o'
+                                scroll={false}
+                                >
+                                    <button
+                                    className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+                                    >
+                                    Dashboard
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                }
                 {/**<Utilities/>*/}
             </div>
        </main>
