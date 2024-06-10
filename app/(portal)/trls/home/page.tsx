f24b95b28b59ab9cf501564147709d1d1f771fc6
bopaki <btebalo@gmail.com>
2024-06-10 13:48:23 +0200

    removed console log

--- app/(portal)/trls/home/page.tsx
+++ app/(portal)/trls/home/page.tsx
@@ -25,7 +25,6 @@ export default async function Home(){
     //         break;
     //     }
     // }
-    console.log('home', session?.user?.realm_access?.roles[0])
     if(!session?.user?.realm_access){
         redirect('/welcome')
     }
