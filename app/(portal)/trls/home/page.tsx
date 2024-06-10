0be64a8eeb902594c7144d67417ee1eaf1a39b9e
bopaki <btebalo@gmail.com>
2024-06-10 13:29:49 +0200

    pushed without tests - ip issue

--- app/(portal)/trls/home/page.tsx
+++ app/(portal)/trls/home/page.tsx
@@ -16,7 +16,14 @@ import { redirect } from "next/navigation";
 export default async function Home(){
     const session = await getSession();
     // const userRole = session?.user?.roles[0]
-    const userRole = session?.user?.realm_access?.roles[0]
+    const roles = ['REGISTRATION_OFFICER','SNR_REGISTRATION_OFFICER', 'MANAGER', 'DIRECTOR', 'REGISTRAR', 'LICENSE_OFFICER', 'SNR_LICENSE_OFFICER', 'LICENSE_MANAGER', 'ADMIN']
+    // const userRole = session?.user?.realm_access?.roles[0]
+    let userRole = ''
+    for(const role in session?.user?.realm_access?.roles){
+        if(role in roles){
+         return userRole = role
+        }
+    }
     console.log('home', session?.user?.realm_access?.roles[0])
     if(!session?.user?.realm_access){
         redirect('/welcome')
