c2e74990a6f2e826d3d76f9fe04bfab2a838480c
bopaki <btebalo@gmail.com>
2024-06-11 14:31:28 +0100

    test prod

--- middleware.ts
+++ middleware.ts
@@ -5,20 +5,20 @@ export async function  middleware(request:NextRequest) {
     //return await updateSession(request);
     const session = await getSession();
 
-    const roles = ['REGISTRATION_OFFICER', 'SNR_REGISTRATION_OFFICER', 'MANAGER', 'DIRECTOR', 'REGISTRAR', 'LICENSE_OFFICER', 'SNR_LICENSE_OFFICER', 'LICENSE_MANAGER', 'ADMIN'];
+    // const roles = ['REGISTRATION_OFFICER', 'SNR_REGISTRATION_OFFICER', 'MANAGER', 'DIRECTOR', 'REGISTRAR', 'LICENSE_OFFICER', 'SNR_LICENSE_OFFICER', 'LICENSE_MANAGER', 'ADMIN'];
     
-    let userRole = '';
+    // let userRole = '';
 
-    for(const role of session?.user?.realm_access?.roles || []){
-        if(roles.includes(role)){
-            userRole = role;
-            break;
-        }
-    }
+    // for(const role of session?.user?.realm_access?.roles || []){
+    //     if(roles.includes(role)){
+    //         userRole = role;
+    //         break;
+    //     }
+    // }
 
-    if(userRole==='' && !request?.nextUrl?.pathname?.startsWith('/welcome')){
+    if(!session && !request?.nextUrl?.pathname?.startsWith('/welcome')){
       return Response.redirect(new URL('/welcome', request.url))
-    }else if(session?.user?.access && (request?.nextUrl?.pathname === '/')){
+    }else if(session?.user?.realm_access && (request?.nextUrl?.pathname === '/')){
       return Response.redirect(new URL('/trls//home', request.url))
     }
 }
