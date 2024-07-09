21a53f26ec61fa143fd778c27744a810ebff2f42
bopaki <btebalo@gmail.com>
2024-07-09 01:25:21 +0100

    rem

--- app/(portal)/trls/settings/page.tsx
+++ app/(portal)/trls/settings/page.tsx
@@ -2,7 +2,6 @@ export const dynamic = 'force-dynamic';
 import { Separator } from "@/components/ui/separator";
 import { ProfileForm } from "./profile-form";
 
-export const dynamic = 'force-dynamic';
 export default function SettingsProfilePage() {
   return (
     <div className="space-y-6">
