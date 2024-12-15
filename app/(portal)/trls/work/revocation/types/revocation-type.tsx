0fd470a974568a3393dce48b6067ab850afa77e2
bopaki <btebalo@gmail.com>
2024-12-15 23:09:17 +0200

    fixed dummy data

--- app/(portal)/trls/work/revocation/types/revocation-type.tsx
+++ app/(portal)/trls/work/revocation/types/revocation-type.tsx
@@ -16,19 +16,20 @@ interface profile {
 }
 
 export interface revocation {
+    id: number | null,
     revocation_number: string | null,
     userid: string | null,
     sla: string | null,
     registration_number: string | null,
     current_employer: string | null,
+    application_id: string | null,
     employer_contact: string | null,
     declaration: string | null,
     profile_data_consent: boolean,
     reason: string | null,
     reg_status: string | null,
-    date_of_submission: string | null,
     created_at: string | null,
-
+    updated_at: string | null,
 }
 
 export interface RevocationResponse {
