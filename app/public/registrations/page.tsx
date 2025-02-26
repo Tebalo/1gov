cac0a39e7c07ff970b38f83822b9bda71c282364
bopaki <btebalo@gmail.com>
2025-02-26 23:19:13 +0200

    Update public report page

--- app/public/registrations/page.tsx
+++ app/public/registrations/page.tsx
@@ -1,8 +1,5 @@
-'use client';
-
 import React from 'react';
-
-
+import TeacherRegistrationReport from './components/teacher-registration-report';
 
 const RegistrationsPage = () => {
   return (
@@ -10,6 +7,7 @@ const RegistrationsPage = () => {
       <h1 className="text-2xl font-bold mb-4">Registrations</h1>
       <div className="bg-white rounded-lg shadow p-6">
         <p>Welcome to the registrations page.</p>
+        <TeacherRegistrationReport/>
       </div>
     </div>
   );
