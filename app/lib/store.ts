52a08ceea2da7f1d60dcf80bac2ce44ac642ebb3
bopaki <btebalo@gmail.com>
2025-10-12 15:27:47 +0200

    Updated TRLS IAM urls

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -29,7 +29,7 @@ const baseURLs = {
         fileUploadUrl: 'https://twosixdigitalbw.com/api/v2-uat/document',
         iamBaseUrl: 'https://gateway-cus-acc.gov.bw',
         iamAuthBaseURL: 'https://gateway-cus-acc.gov.bw/v2',
-        trlsAuthBaseURL: 'https://twosixdigitalbw.com'
+        trlsAuthBaseURL: 'https://twosixdigitalbw.com/v1-uat'
     },
     production: {
         trls: 'http://10.0.26.174',
@@ -39,7 +39,7 @@ const baseURLs = {
         fileUploadUrl: 'https://twosixdigitalbw.com/api/v2/document',
         iamBaseUrl: 'https://gateway-cus.gov.bw',
         iamAuthBaseURL: 'https://gateway-cus.gov.bw',
-        trlsAuthBaseURL: 'https://twosixdigitalbw.com'
+        trlsAuthBaseURL: 'https://twosixdigitalbw.com/v1'
     }
 }
 // const currentBaseURLs = baseURLs[env as 'uat' | 'production'];
